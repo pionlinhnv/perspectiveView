906f7d5c9b08327e74e7335aac0c6dae1cc1e186
pionlinhnv <linhnv@piontech.co>
2025-05-26 15:03:03 +0700

    linhnv: draw bound, center

--- app/src/main/java/pion/tech/pionbase/customview/SelectToolView.kt
+++ app/src/main/java/pion/tech/pionbase/customview/SelectToolView.kt
@@ -1,12 +1,14 @@
 package pion.tech.pionbase.customview
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.DashPathEffect
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.Rect
+import android.graphics.RectF
+import android.graphics.Region
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -20,28 +22,58 @@ class SelectToolView @JvmOverloads constructor(
     private val selectPaint = Paint()
     private val selectPath = Path()
 
-    private val bitmapResult: Bitmap? = null
+    //tam cua path
+    private val centerPaint = Paint().apply {
+        color = Color.parseColor("#CC3A2B")
+        style = Paint.Style.FILL
+    }
+
+    //bound cua path
+    private val boundPaint = Paint().apply {
+        color = Color.WHITE
+        style = Paint.Style.STROKE
+    }
 
     init {
         selectPaint.apply {
             color = Color.BLACK
             style = Paint.Style.STROKE
             strokeWidth = context.convertDpToPixel(2f)
-                pathEffect = DashPathEffect(floatArrayOf(20f, 10f), 0f)
+            pathEffect = DashPathEffect(floatArrayOf(20f, 10f), 0f)
         }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         drawSelectPath(canvas)
+        drawCenter(canvas)
+        drawBound(canvas)
     }
 
     private fun drawSelectPath(canvas: Canvas) {
         canvas.drawPath(selectPath, selectPaint)
     }
 
+    private fun drawCenter(canvas: Canvas) {
+        val bounds = RectF()
+        selectPath.computeBounds(bounds, true)
+
+        // Tọa độ tâm
+        val centerX = bounds.centerX()
+        val centerY = bounds.centerY()
+
+        canvas.drawCircle(centerX, centerY, 10f, centerPaint)
+    }
+
+    private fun drawBound(canvas: Canvas) {
+        val bounds = RectF()
+        selectPath.computeBounds(bounds, true)
+
+        canvas.drawRect(bounds, boundPaint)
+    }
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        when(event.action) {
+        when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 selectPath.reset()
                 selectPath.moveTo(event.x, event.y)
